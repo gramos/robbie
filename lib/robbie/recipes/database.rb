fcff5d42af7e0c1192958361753731a931e95713
Gaston Ramos <ramos.gaston@gmail.com>
2008-10-07 12:29:32 -0300

    added more recipes to database

--- lib/robbie/recipes/database.rb
+++ lib/robbie/recipes/database.rb
@@ -7,7 +7,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       backup_time = [now.year,now.month,now.day,now.hour,now.min,now.sec].join('-')
       set :backup_file, "#{shared_path}/db_backups/#{environment_database}-snapshot-#{backup_time}.sql"
     end
-  
+
     desc "Clone Production Database to Staging Database."
     task :clone_prod_to_staging, :roles => :db, :only => { :primary => true } do
       backup_name
@@ -22,7 +22,7 @@ Capistrano::Configuration.instance(:must_exist).load do
       end
       run "rm -f #{backup_file}"
     end
-  
+
     desc "Backup your MySQL or PostgreSQL database to shared_path+/db_backups"
     task :dump, :roles => :db, :only => {:primary => true} do
       backup_name
@@ -33,7 +33,7 @@ Capistrano::Configuration.instance(:must_exist).load do
         run "PGPASSWORD=#{dbpass} pg_dump -c -U #{dbuser} -h #{environment_dbhost} #{environment_database} | bzip2 -c > #{backup_file}.bz2"
       end
     end
-    
+
     desc "Sync your production database to your local workstation"
     task :clone_to_local, :roles => :db, :only => {:primary => true} do
       backup_name
@@ -48,5 +48,51 @@ Capistrano::Configuration.instance(:must_exist).load do
       %x!#{run_str}!
     end
   end
+
+###########################################################################
+# DATABASE YAML
+
+# after "deploy:setup", :db
+# after "deploy:update_code", "db:symlink"
+
+namespace :db do
+
+  desc "Create database yaml in shared path"
+  task :default do
+    db_config = ERB.new <<-EOF
+    base: &base
+      adapter: mysql
+      username: #{runner}
+      password: #{password}
+      host: 127.0.0.1
+
+    development:
+      database: #{db_name}_dev
+      <<: *base
+
+    test:
+      database: #{db_name}_test
+      <<: *base
+
+    production:
+      database: #{db_name}_prod
+      shooting_star:
+        server: #{shoot_server}:#{shoot_server_port}
+        shooter: druby://localhost:#{shoot_drb_port}
+      <<: *base
+    EOF
+
+    sudo "mkdir -p #{shared_path}/config"
+    sudo "chown -R #{user}:#{mongrel_group} #{deploy_to}/"
+    sudo "chmod 774 -R #{shared_path}/config"
+    put db_config.result, "#{shared_path}/config/database.yml"
+  end
+
+  desc "Make symlink for database yaml"
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+
+end
 
 end
